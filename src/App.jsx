47a409c4d90dc8178c24ffbe7320ab8f8c968ea7
Bill Juompan <boris.goummo@cm.maviance.com>
2025-07-08 18:34:10 +0100

    useReducer practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import { useState, useEffect, useContext, createContext, useRef } from 'react';
+import { useState, useEffect, useContext, createContext, useRef, useReducer } from 'react';
 import './App.css';
 
 // useContext: Access Context in a Function Component
 const ThemeContext = createContext();
-const theme = useContext(ThemeContext);
 
+function customReducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + action.value };
+    case "substract":
+      return { count: state.count - action.value };
+  
+    default:
+      return state;
+  }
+
+}
 function App() {
   // useState: Add State to a Function Component
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  const initialState = { count: 0 };
+  // Using useReducer to handle complex state modifications logic
+  const [state, dispatch] = useReducer(customReducer, initialState);
 
   // useRef: Access DOM Elements
   const inputRef = useRef(null);
 
   // useEffect: Side Effects
   useEffect(() => {
-    console.log(`Value of count changed: ${count}`);
+    console.log(`Value of count changed: ${state.count}`);
 
-    // Bind the new value to the input element
     if (inputRef.current) {
-      inputRef.current.value = `Count is ${count}`;
+      inputRef.current.value = `Count is ${state.count}`;
     }
-  }, [count]);
+  }, [state.count]);
+
+  
 
   return (
     <>
@@ -28,17 +44,21 @@ function App() {
         How are you doing ?
       </div>
       <br />
-      <button onClick={() => { console.log("Clicked !"); setCount(count + 1) }}>Click me here {count}</button>
+      <button onClick={() => dispatch({type: "add", value: 10})}>Increment {state.count}</button>
+      <button onClick={() => dispatch({type: "substract", value: 10})}>Decrement {state.count}</button>
       <ThemeContext.Provider value="dark">
         {/* I saw that this supplies value to any child component that uses useContext */}
-        <div>
-          Current Theme: {theme}
-        </div>
+        <ThemeDisplay />
       </ThemeContext.Provider>
 
       <input type="text" ref={inputRef}/>
     </>
   )
+}
+
+function ThemeDisplay() {
+  const theme = useContext(ThemeContext);
+  return <div>Current Theme: {theme}</div>;
 }
 
 export default App;
